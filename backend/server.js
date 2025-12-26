1acf319f2df61bf64da9d5892bb30527efc160ce
Ansh Kumar <anshresostudent123@gmail.com>
2025-12-27 00:42:14 +0530

    Database connected

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ dotenv.config();
 
 import http from "http";
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
+
+// connect database
+connectDB();
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
